28ad33821eaa4c7cb4df00a75b35b1cb5710ef9f
Mackenzie <mackenziernelson@gmail.com>
2017-12-28 16:32:50 -0500

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_vqzg834a1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ojhegp3nq7jsk96cv59t2oud7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
